d8de259f03fb2b497b4a87c6964d56a6f9b2c7b0
MUAmaiz-asleep <syedmumaiz28@gmail.com>
2026-03-30 11:23:48 +0000

    DataType

---
+++ 01_basic/DataType.js
@@ -0,0 +1,24 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+// alert( 3 + 3) // we are using nodejs, not browser
+
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3) // code readability should be high
+
+console.log("momo")
+
+
+let name = "momo"
+let age = 17
+let isLoggedIn = false
+let state;
+
+console.log(typeof undefined);
+console.log(typeof null);
