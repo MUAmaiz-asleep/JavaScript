ac7ae59d728218f43052bd153eff2b20a2c7bd25
momo-inaction <syedmumaiz28@gmail.com>
2026-04-04 14:43:48 +0000

    just for green dot

---
+++ 01_basic/04_comparisons.js
@@ -0,0 +1,22 @@
+// x = 3 >+ 1
+// console.log(x);
+// console.log(3 >= 1);
+//  console.log(3 == 3);
+// console.log(3 != 1);
+
+//  console.log("3" > 3);
+// console.log("03" > 5);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null == 0); 
+// console.log(null >= 0);
+// console.log(null >+ 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// for operator === (strict check)
+
+console.log("3" === 3);
