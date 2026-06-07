cc49d1f9726b87776cae77cc0b3f85f884e309ef
momo-inaction <syedmumaiz28@gmail.com>
2026-06-07 18:31:14 +0000

    time done

---
+++ 01_basic/04_object-random.js
@@ -0,0 +1,26 @@
+// // singleton
+// //  Object.create
+
+// //  object literals
+
+// const mySym = Symbol("key1")
+
+// const JsUser = {name:"MOMO" , mySym: "mykey1", age: 18, location: "Hyderabad", email: "momo@gmail.com", isLoggedIn: false, lastLoginDays: ["Monday", "SAturday"] }
+
+// console.log(JsUser)
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser.mySym)
+
+
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "134abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const
