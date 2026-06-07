5a2c957a2ca331c541bed2c4dc89c157874be1fc
momo-inaction <syedmumaiz28@gmail.com>
2026-06-07 18:56:55 +0000

    done for now

--- 01_basic/04_object-random.js
+++ 01_basic/04_object-random.js
@@ -23,4 +23,45 @@ tinderUser.isLoggedIn = false
 
 // console.log(tinderUser)
 
-const
+const regularUser = {
+    email: "someoneat.com",
+    fullname: {
+        userfullname: {
+            firstname: "momo",
+            lastname:"why"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname.lastname)
+
+const obj1 = {1: "a", 3: "b"}
+const obj3 = {4: "a", 5: "b"}
+const obj5 = {6: "a", 7: "b"}
+
+// const onj4 = {obj1 , obj3}
+// const obj4 = Object.assign({}, obj1, obj5)
+
+const obj4 = {...obj1, ...obj3}
+// console.log(obj3)
+
+
+
+const users = [
+      {
+        id: 1,
+        email: "atgamil.com" 
+    },
+    {
+        id: 1,
+        email: "atgamil.com" 
+    },
+    {
+        id: 1,
+        email: "atgamil.com" 
+    },
+]
+
+users[1].email
+console.log(tinderUser)
+console.log(Objects.keys(tinderUser))
