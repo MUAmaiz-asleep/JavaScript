04e0c9717849a81c8ac1a4903dc3fcffc5c582bd
momo-inaction <syedmumaiz28@gmail.com>
2026-06-16 18:12:44 +0000

    .......

--- 01_basic/06_scopes-random.js
+++ 01_basic/06_scopes-random.js
@@ -20,3 +20,22 @@ function one(){
         console.log(username);
     }
     console.log(website);
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
